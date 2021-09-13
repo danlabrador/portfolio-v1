191444286afa430d315a15852e566c0847ec7fb2
Daniel Labrador <thonydaniellelabrador@gmail.com>
2021-09-13 16:50:05 +0800

    create roadmap and decorative h2 template

---
+++ resources/js/index.js
@@ -0,0 +1,27 @@
+// Roadmap grid box positioning and design -------------------------------------
+
+// Alternate box positioning to grid: right and left
+const milestoneBox = document.getElementsByClassName('milestone-box');
+for (let i=0; i<milestoneBox.length; ++i){
+    if (i%2==1){
+        milestoneBox.item(i).className += ' milestone-box-left';
+    } else if (i%2==0){
+        milestoneBox.item(i).className += ' milestone-box-right';
+    }
+}
+
+const milestoneBoxR = document.getElementsByClassName('milestone-box-right');
+for(let i=0; i<milestoneBoxR.length; ++i){
+    milestoneBoxR.item(i).style.gridRowStart = 2*i+1;
+}
+
+const milestoneBoxL = document.getElementsByClassName('milestone-box-left');
+for(let i=0; i<milestoneBoxL.length; ++i){
+    milestoneBoxL.item(i).style.gridRowStart = 2*i+2;
+}
+
+// Increase line before milestoneBtn
+milestoneBox.item(milestoneBox.length-1).style.paddingBottom = '3.75em';
+
+const milestoneBtn = document.getElementById('milestone-button');
+milestoneBtn.style.gridRowStart = milestoneBox.length+1;
